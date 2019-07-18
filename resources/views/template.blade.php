6fc760857fa23ebab3b4a29a7bc5154d889f1428
Zulibad <zulfikar.ibad@gmail.com>
2019-07-18 14:38:00 +0700

    update 1

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -1,11 +1,20 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Siswaku</title>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-widht, initial-scale=1">
+
+     <title>Siswaku</title>
+        <link herf="{{ asset ('bootstrap_3_3_6/css/bootstrap.min.css') }}" rel="stylesheet">
+        <link href="{{ asset ('css/style.css') }}" rel="stylesheet">
     </head>
     <body>
-        @yield('main')
-
-        @yield('footer')
+        <div class="container">
+            @yield('main')
+        </div>
+            @yield('footer')
+        <script src="{{ asset ('js/jquery_2_2_1.min.js') }}"> </script>
+        <script src="{{ asset ('bootstrap_3_3_6/js/bootstrap.min.js') }}"> </script>
     </body>
 </html>
