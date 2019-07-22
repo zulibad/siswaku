0ae8a3f2b7c0a1a8f4d90be029ac9ca978764293
Zulibad <zulfikar.ibad@gmail.com>
2019-07-22 22:51:47 +0700

    Penambahan method store, untuk request penambahan siswa

--- app/Http/Controllers/SiswaController.php
+++ app/Http/Controllers/SiswaController.php
@@ -19,4 +19,9 @@ class SiswaController extends Controller
     public function create() {
         return view('siswa.create');
     }
+
+    public function store(Request $request) {
+        $siswa = $request->all();
+        return $siswa;
+    }
 }
