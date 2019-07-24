0eaafda80e7f920141165e9c8bc0121c97827c3d
Zulibad <zulfikar.ibad@gmail.com>
2019-07-25 06:11:37 +0700

    blade baru show untuk detail siswa

---
+++ resources/views/siswa/show.blade.php
@@ -0,0 +1,36 @@
+@extends('template')
+
+@section('main')
+
+        <div id="siswa">
+            <h2>Detail Siswa</h2>
+
+            <table class="table table-striped">
+                <tr>
+                    <th>NISN</th>
+                    <td>{{ $siswa->nisn }}</td>
+                </tr>
+
+                <tr>
+                    <th>Nama</th>
+                    <td>{{ $siswa->nama_siswa }}</td>
+                </tr>
+
+                <tr>
+                    <th>Tanggal Lahir</th>
+                    <td>{{ $siswa->tanggal_lahir }}</td>
+                </tr>
+
+                <tr>
+                    <th>Jenis Kelamin</th>
+                    <td>{{ $siswa->jenis_kelamin }}</td>
+                </tr>
+            </table>
+        </div>
+@stop
+
+@section('footer')
+    <div id="footer">
+        <p>&copy; 2019 siswaku.app</p>
+    </div>
+@stop
