3ecdeeb853b090ef75026a21469dc55e27c87660
Zulibad <zulfikar.ibad@gmail.com>
2019-07-27 14:50:57 +0700

    menghapus isi coding form, dan menambah @include

--- resources/views/siswa/create.blade.php
+++ resources/views/siswa/create.blade.php
@@ -2,39 +2,10 @@
 
 @section('main')
     <div id="siswa">
-        <h2> Tambah Siswa</h2>
+        <h2> Tambah Anak Asuh</h2>
 
         {!! Form::open(['url' => 'siswa']) !!}
-            <div class="form-group">
-                {!! Form::label('nisn', 'NISN:', ['class' => 'control-label']) !!}
-                {!! Form::text('nisn', null, ['class' => 'form-control']) !!}
-            </div>
-
-            <div class="form-group">
-                {!! Form::label('nama_siswa', 'Nama:', ['class' => 'control-label']) !!}
-                {!! Form::text('nama_siswa', null, ['class' => 'form-control']) !!}
-            </div>
-
-            <div class="form-group">
-                {!! Form::label('tanggal_lahir', 'Tanggal Lahir:', ['class' => 'control-label']) !!}
-                {!! Form::date('tanggal_lahir', null, ['class' => 'form-control', 'id' => 'tanggal_lahir']) !!}
-            </div>
-
-            <div class="form-group">
-                {!! Form::label('jenis_kelamin', 'Jenis Kelamin:', ['class' => 'control-label']) !!}
-                    <div class="radio">
-                        <label>{!! Form::radio('jenis_kelamin', 'L') !!} Laki-Laki</label>
-                    </div>
-                    
-                    <div class="radio">
-                        <label>{!! Form::radio('jenis_kelamin', 'P') !!} Perempuan</label>
-                    </div>
-            </div>
-
-            <div class="form-group">
-                {!! Form::submit('Tambah Siswa', ['class' => 'btn btn-primary form-control']) !!}
-            </div>
-
+        @include('siswa.form', ['submitButtonText' => 'Simpan'])
         {!! Form::close() !!}
     </div>
 @stop
