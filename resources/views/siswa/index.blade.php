c560d5c2613be9516124759ca9b4574eb302aa23
Zulibad <zulfikar.ibad@gmail.com>
2019-07-31 09:53:54 +0700

    merubah table-nav

--- resources/views/siswa/index.blade.php
+++ resources/views/siswa/index.blade.php
@@ -26,8 +26,17 @@
                                 <div class="box-button">
                                     {{ link_to('siswa/'.$siswa->id, 'Detail', ['class' => 'btn btn-primary btn-sm']) }}
                                 </div>
-                                
-                                {{ link_to('siswa/'.$siswa->id. '/edit', 'Edit', ['class' => 'btn btn-warning btn-sm']) }}</td>
+
+                                <div class="box-button">
+                                {{ link_to('siswa/'.$siswa->id. '/edit', 'Edit', ['class' => 'btn btn-warning btn-sm']) }}
+                                </div>
+
+                                <div class="box-button">
+                                {!! Form::open(['method'=>'DELETE', 'action'=>['SiswaController@destroy', $siswa->id]]) !!}
+                                {!! Form::submit('Delete', ['class'=> 'btn btn-danger btn-sm']) !!}
+                                {!! Form::close() !!}
+                                </div>
+                            </td>
                         </tr>
                         <?php endforeach ?>
                     </tbody>
@@ -36,20 +45,17 @@
                 <p>Tidak ada data siswa.</p>
             @endif
 
-            <div class="table-bottom">
-                <div class="pull-left">
-                    <strong>Jumlah Siswa : {{ $jumlah_siswa }}</strong>
+            <div class="table-nav">
+                <div class="jumlah-data">
+                     <strong> Jumlah Siswa: {{ $jumlah_siswa }} </strong>
                 </div>
-
-                <div class="pull-right">
-                    Pagination
+                <div class="paging">
+                    {{ $siswa_list->links() }}
                 </div>
             </div>
 
-            <div class="bottom-nav">
-                <div>
-                    <a href="siswa/create" class="btn btn-primary">Tambah Siswa</a>
-                </div>
+            <div class="tombol-nav">
+                <a href="siswa/create" class="btn btn-primary">Tambah Siswa</a>
             </div>
         </div>
 @stop
