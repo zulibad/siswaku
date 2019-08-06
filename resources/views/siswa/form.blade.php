3a67d580dec12aab9a7026734969d3e96807e721
Zulibad <zulfikar.ibad@gmail.com>
2019-08-06 16:56:21 +0700

    menambah form eror, warna dari bootstrap

--- resources/views/siswa/form.blade.php
+++ resources/views/siswa/form.blade.php
@@ -1,19 +1,48 @@
-<div class="form-group">
+@if (isset($siswa))
+    {!! Form::hidden('id', $siswa->id) !!}
+@endif
+
+@if ($errors->any())
+    <div class="form-group {{ $errors->has('nisn') ? 'has-error' : 'has-success' }}">
+@else
+    <div class="form-group">
+@endif
     {!! Form::label('nisn', 'NISN:', ['class' => 'control-label']) !!}
     {!! Form::text('nisn', null, ['class' => 'form-control']) !!}
+        @if ($errors->has('nisn'))
+        <span class="help-block">{{ $errors->first('nisn') }}</span>
+        @endif
 </div>
 
-<div class="form-group">
+@if ($errors->any())
+    <div class="form-group {{ $errors->has('nama_siswa') ? 'has-error' : 'has-success' }}">
+@else
+    <div class="form-group">
+@endif
     {!! Form::label('nama_siswa', 'Nama:', ['class' => 'control-label']) !!}
     {!! Form::text('nama_siswa', null, ['class' => 'form-control']) !!}
+        @if ($errors->has('nama_siswa'))
+        <span class="help-block">{{ $errors->first('nama_siswa') }}</span>
+        @endif
 </div>
 
-<div class="form-group">
+@if ($errors->any())
+    <div class="form-group {{ $errors->has('tanggal_lahir') ? 'has-error' : 'has-success' }}">
+@else
+    <div class="form-group">
+@endif
     {!! Form::label('tanggal_lahir', 'Tanggal Lahir:', ['class' => 'control-label']) !!}
-    {!! Form::date('tanggal_lahir', null, ['class' => 'form-control', 'id' => 'tanggal_lahir']) !!}
+    {!! Form::date('tanggal_lahir', !empty($siswa) ? $siswa->tanggal_lahir->format('Y-m-d'): null, ['class' => 'form-control', 'id' => 'tanggal_lahir']) !!}
+        @if ($errors->has('tanggal_lahir'))
+        <span class="help-block">{{ $errors->first('tanggal_lahir') }}</span>
+        @endif
 </div>
 
-<div class="form-group">
+@if ($errors->any())
+    <div class="form-group {{ $errors->has('jenis_kelamin') ? 'has-error' : 'has-success' }}">
+@else
+    <div class="form-group">
+@endif
     {!! Form::label('jenis_kelamin', 'Jenis Kelamin:', ['class' => 'control-label']) !!}
         <div class="radio">
             <label>{!! Form::radio('jenis_kelamin', 'L') !!} Laki-Laki</label>
@@ -22,6 +51,10 @@
         <div class="radio">
             <label>{!! Form::radio('jenis_kelamin', 'P') !!} Perempuan</label>
         </div>
+
+        @if ($errors->has('jenis_kelamin'))
+        <span class="help-block">{{ $errors->first('jenis_kelamin') }}</span>
+        @endif
 </div>
 
 <div class="form-group">
