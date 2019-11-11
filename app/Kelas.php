111b39fcded2ddcc8275eb9ab57af6420ad5d594
Zulibad <zulfikar.ibad@gmail.com>
2019-11-11 16:48:39 +0700

    menambah model kelas

---
+++ app/Kelas.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Kelas extends Model
+{
+    protected $table = 'kelas';
+    protected $fillable = ['nama_kelas'];
+    public function siswa() {
+        return $this->hasMany('App\Siswa', 'id_kelas');
+    }
+}
