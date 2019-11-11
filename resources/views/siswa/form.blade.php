91cb8b66062ee7db29dde931572f2470bdeee029
Zulibad <zulfikar.ibad@gmail.com>
2019-11-11 16:52:38 +0700

    menambah form isian kelas

--- resources/views/siswa/form.blade.php
+++ resources/views/siswa/form.blade.php
@@ -39,6 +39,23 @@
     </div>
 
 @if ($errors->any())
+    <div class="form-group {{ $errors->has('id_kelas') ? 'has-error' : 'has-success' }}">
+@else
+    <div class="form-group">
+@endif
+        {!! Form::label('id_kelas', 'Kelas:', ['class' =>'control-label']) !!}
+            @if (count ($list_kelas) > 0)
+                {!! Form::select('id_kelas', $list_kelas, null, ['class' => 'form-control', 'id' => 'id_kelas', 'placeholder' => 'Pilih Kelas']) !!}
+            @else
+                <p>Tidak ada pilihan kelas, buat dulu ya .!</p>
+            @endif
+            
+            @if ($errors->has('id_kelas'))
+                <span class="help-block">{{ $errors->first('id_kelas')}} </span>
+            @endif
+    </div>
+
+@if ($errors->any())
     <div class="form-group {{ $errors->has('jenis_kelamin') ? 'has-error' : 'has-success' }}">
 @else
     <div class="form-group">
